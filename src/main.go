9b6aee30cd8783c864287292ac2f27055f04db8e
merveealpay <merveealpay@gmail.com>
2020-03-20 14:56:51 +0300

    path example

--- src/main.go
+++ src/main.go
@@ -5,7 +5,21 @@ import (
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Index Page"))
+	//w.Write([]byte("Index Page"))
+	//w.WriteHeader(http.StatusOK)
+
+	//burada localhost:8080'den sonra yazdıgımız degeri pathVar' olarak aldım
+	pathVar := r.URL.Path[1:]
+	data := ""
+
+	if len(pathVar) > 0 {
+		data = "Path: " + pathVar
+	} else {
+		data = "Index Page"
+	}
+
+	w.Write([]byte(data))
+
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
