300f1de0bc7c6a6e00d9a4fa4527b753a0aaa823
Onur Kağan ALDEMİR <onurkaganaldemir@gmail.com>
2020-03-30 16:14:35 +0300

    ex-2

--- src/ex-2/main.go
+++ src/ex-2/main.go
@@ -1,30 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 )
 
-//mux, bir kere olustur bunun uzerınden handle et.
+type hero struct {
+	ID          int
+	Name, Quote string
+}
+
+var (
+	ironman = hero{
+		ID:    1,
+		Name:  "ironman",
+		Quote: "Sometimes you gotta run before you can walk.",
+	}
+	wolverine = hero{
+		ID:    1,
+		Name:  "wolverine",
+		Quote: "Nature made me a freak. Man made me a weapon. And god made it last too long.",
+	}
+)
+
 func main() {
-
-	var i ironman
-	var w wolverine
-
 	mux := http.NewServeMux()
-	mux.Handle("/ironman", i)
-	mux.Handle("/wolverine", w)
+
+	mux.Handle("/ironman", ironman)
+	mux.Handle("/wolverine", wolverine)
+
 	http.ListenAndServe(":8080", mux)
 }
 
-type ironman int
-
-func (x ironman) ServeHTTP(res http.ResponseWriter, r *http.Request) {
-	io.WriteString(res, "Mr.Iron!")
-}
-
-type wolverine int
-
-func (x wolverine) ServeHTTP(res http.ResponseWriter, r *http.Request) {
-	io.WriteString(res, "Mr.Wolverine!")
+func (hero hero) ServeHTTP(res http.ResponseWriter, r *http.Request) {
+	io.WriteString(res, fmt.Sprintf("%s : %s", hero.Name, hero.Quote))
 }
